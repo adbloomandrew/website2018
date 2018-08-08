59be78c91db9293c61ef740988ed424777513296
Serhii Hulko <serj.hulko@gmail.com>
2018-08-08 21:13:34 +0300

    fixed contact php script

--- app/contact.php
+++ app/contact.php
@@ -1,17 +1,5 @@
 <?php
 
- //Check for empty fields
-if(empty($_POST['name'])     ||
-   empty($_POST['email'])    ||
-   empty($_POST['url'])      ||
-   empty($_POST['company'])  ||
-   empty($_POST['message'])  ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-    return false;
-   }
-
 $name = $_POST['name'];
 $email = $_POST['email'];
 $url = $_POST['url'];
@@ -20,8 +8,7 @@ $message = $_POST['message'];
 
 // Create the email and send the message
 
-// $to = "andrew@adbloom.com";
-$to = "gulkobox@gmail.com";
+$to = "andrew@adbloom.com";
 
 $subject = "New message from $name";
 $content = "You have received a new message from http://www.adbloom.com learn more form.\n\n Name: $name \n\n Email: $email \n\n URL: $url \n\n Company: $company \n\n Message: \n\n $message";
