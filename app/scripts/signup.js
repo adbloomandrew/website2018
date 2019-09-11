ffdf9648d11ad0a920fa636f9a963ae0be8f4368
Serhii Hulko <serj.hulko@gmail.com>
2019-09-11 18:15:41 +0300

    fixed signing up and added error messages

--- app/scripts/signup.js
+++ app/scripts/signup.js
@@ -1,20 +1,47 @@
 $(document).ready(function() {
   var form = $("form#signUpForm")[0];
+
   $(form).submit(function (e) {
     e.preventDefault();
     if(form.checkValidity()) {
+      toggleLoader();
       $.ajax({
-        url: "https://my.adbloom.co/sigp/",
+        url: "https://my.adbloom.co/signup/",
         type: "post",
-        dataType: "json",
+        dataType: "text",
         data: $(form).serialize(),
         success: function(data) {
-
+          parseResponse(data);
+          toggleLoader();
         },
         error: function (error) {
-
+          console.error(error);
+          showErrorMessages("Oops! Something went wrong. Try again later");
+          toggleLoader();
         }
       });
     }
   });
 });
+
+function toggleLoader() {
+  $("div#loader-animation").toggleClass("d-none");
+}
+
+function parseResponse(data) {
+  var userMessage = $(data).find("div#user-messages");
+  if(!!data) {
+    if(!!userMessage.length) {
+      var errorList = $(userMessage).find("ul").addClass("error-messages-list");
+      showErrorMessages(errorList);
+    } else {
+      window.location.href = "/login/";
+    }
+  }
+}
+
+function showErrorMessages(message) {
+  var responseMessage = $("div#response-message");
+  $(responseMessage).removeClass("d-none").html(message);
+  $(window).scrollTop(0);
+}
